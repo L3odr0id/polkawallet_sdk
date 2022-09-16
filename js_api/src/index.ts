146b344bd9145dd1c1a694b7966f99b25e8aede5
L3odr0id <leon.leonc@yandex.ru>
2022-09-16 17:15:57 +0300

    Try RPC_TYPES

--- js_api/src/index.ts
+++ js_api/src/index.ts
@@ -35,6 +35,27 @@ async function connect(nodes: string[]) {
     try {
       const res = await ApiPromise.create({
         provider: wsProvider,
+        types:{
+          AccountInfo: "AccountInfoWithTripleRefCount",
+          Address: "AccountId",
+          LookupSource: "AccountId",
+          Keys: "SessionKeys2",
+          Weight: "u32",
+          Difficulty: "u256",
+          DifficultyAndTimestamp: {
+            difficulty: "Difficulty",
+            timestamp: "u64",
+          },
+          LockParameters: {
+            period: "u16",
+            divide: "u16",
+          },
+          StorageVersion: {
+            _enum: ["V0", "V1"],
+            V0: "u8",
+            V1: "u8",
+          },
+        },
         metadata: {
           [`${KUSAMA_GENESIS}-9122`]: localMetadata["kusama"],
           [`${POLKADOT_GENESIS}-9122`]: localMetadata["polkadot"],
